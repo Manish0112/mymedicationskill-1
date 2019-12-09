f2e89465c80198d0bd5fe5fe08a9281508e8738b
Bharath Medarametla <bharathchoudary13@gmail.com>
2019-12-09 13:38:24 -0800

    updated tablename

--- my_medication/lambda/custom/helpers/dbHelper.js
+++ my_medication/lambda/custom/helpers/dbHelper.js
@@ -1,6 +1,6 @@
 var AWS = require("aws-sdk");
 AWS.config.update({region: "us-east-1"});
-const tableName = "prescription";
+const tableName = "prescriptions";
 
 var dbHelper = function () { };
 var docClient = new AWS.DynamoDB.DocumentClient();
